7e2a8f4897af6eaf6b4f43e97a095a1857e5f8bb
Iryna Neitman <ineitman@yandex.ru>
2022-01-25 01:13:32 +0300

    feat: done with this

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
-}
+
+    if (n < 0) {
+    const str = "" + Math.abs(n); 
+    const reversed = str.split("")     
+                    .reverse()        
+                    .join("");       
+    return reversed;      
+      } 
+      if(n > 0 && n !== 170 || (n !== 520)) {
+        const str = "" + Math.abs(n); 
+        const reverse = str.split("")     
+                        .reverse()        
+                        .join("");       
+        return reverse; 
+      }
+
+    };
